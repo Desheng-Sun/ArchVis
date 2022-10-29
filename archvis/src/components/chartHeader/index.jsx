996501e6acf3f32eab88b8f4f922d096a4ce77e3
LXCCXLLXC <xc.luo@qq.com>
2022-10-29 12:50:25 +0800

    lxc

--- archvis/src/components/chartHeader/index.jsx
+++ archvis/src/components/chartHeader/index.jsx
@@ -1,5 +1,11 @@
 import "./index.css";
 
 export default function ChartHeader({ chartName }) {
-  return <div className="chartheader">{chartName}</div>;
+  return (
+    // <div className="chartheader">{chartName}</div>
+    <ul class="nav">
+        <li class="item active">{chartName}</li>
+        <li class="item"></li>
+    </ul>
+  );
 }
